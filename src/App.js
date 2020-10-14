42314c53a5b00f6d8e373631a6c6e2dc49270d8f
Dumitru Mitaru-Berceanu <du@sporkbytes.com>
2020-10-14 10:23:37 -0700

    Change redirectUri for auth0

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
 		<Auth0Provider
 			domain={process.env.REACT_APP_AUTH0_DOMAIN}
 			clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-			redirectUri={`${window.location.origin}/profile`}
+			redirectUri={window.location.origin}
 			audience={process.env.REACT_APP_AUTH0_API_ID}
 			onRedirectCallback={onRedirectCallback}
 		>
