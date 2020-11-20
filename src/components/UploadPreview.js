bc0df696f95aa7583be113e3020dbd235f436b4b
Dumitru Mitaru-Berceanu <du@sporkbytes.com>
2020-11-20 12:16:46 -0800

    Fixes issue where audio preview wouldn't play on chrome

--- src/components/UploadPreview.js
+++ src/components/UploadPreview.js
@@ -4,6 +4,7 @@ import { Button, DialogActions, DialogContent } from '@material-ui/core';
 import Dialog from './Dialog';
 
 const UploadPreview = ({ upload: { url, type, subType }, open, onClose }) => {
+	const mime = type + '/' + subType;
 	return (
 		<Dialog open={open} onClose={onClose}>
 			<DialogContent>
@@ -11,13 +12,20 @@ const UploadPreview = ({ upload: { url, type, subType }, open, onClose }) => {
 					<img src={url} width="100%" height="auto" />
 				)}
 				{type === 'video' && (
-					<video width="100%" height="auto" controls>
-						<source src={url} type={type + '/' + subType} />
+					<video controls width="100%" height="auto" autoPlay>
+						<source
+							src={url}
+							type={!!window.chrome ? undefined : mime}
+						/>
+						Sorry, your browser doesn't support embedded videos.
 					</video>
 				)}
 				{type === 'audio' && (
-					<audio controls>
-						<source src={url} type={type + '/' + subType} />
+					<audio controls autoPlay>
+						<source
+							src={url}
+							type={!!window.chrome ? undefined : mime}
+						/>
 					</audio>
 				)}
 			</DialogContent>
